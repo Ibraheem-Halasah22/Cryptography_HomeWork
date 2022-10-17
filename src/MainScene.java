d0f61510409f228ebf225fbcee26ceb683d63885
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2022-10-17 09:37:20 +0300

    Rails Fence encryption implemented

--- src/MainScene.java
+++ src/MainScene.java
@@ -1,2 +1,25 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class MainScene {
+    @FXML
+    void goToRailsFence(ActionEvent event) throws IOException {
+        ((Stage) ((Node) (event.getSource())).getScene().getWindow()).setScene(new Scene(
+                FXMLLoader.load(Objects.requireNonNull(getClass().getResource("rails_fence_scene.fxml")))
+        ));
+    }
+
+    @FXML
+    void goToLeastSimple(ActionEvent event) throws IOException {
+        ((Stage) ((Node) (event.getSource())).getScene().getWindow()).setScene(new Scene(
+                FXMLLoader.load(Objects.requireNonNull(getClass().getResource("least_simple_scene.fxml")))
+        ));
+    }
 }
--- src/RailsFenceSceneController.java
+++ src/RailsFenceSceneController.java
@@ -1,28 +1,110 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RailsFenceSceneController {
 
 
     @FXML
-    private TextArea plainTextArea;
+    private TextArea decCipherTextArea;
+
+    @FXML
+    private PasswordField decKey;
+
+    @FXML
+    private TextArea decPlainTextArea;
+
+    @FXML
+    private TextArea encCipherTextArea;
+
+    @FXML
+    private PasswordField encKey;
+
+    @FXML
+    private TextArea encPlainTextArea;
+
+    @FXML
+    void bruteForce(ActionEvent event) {
+
+    }
 
     @FXML
     void encrypt(ActionEvent event) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
-        String plainText = plainTextArea.getText().trim();
+        String plainText = encPlainTextArea.getText().trim();
+        int key = 0;
+        try {
+            key = Integer.parseInt(encKey.getText().trim());
+            if (key < 1 || key > 10) {
+                alert.setContentText("Key must be from 1 to 10!");
+                alert.show();
+                return;
+            }
+        } catch (Exception e) {
+            alert.setContentText("Please enter valid numeric key!");
+            alert.show();
+            return;
+        }
         if (plainText.length() == 0) {
             alert.setContentText("The plain text area is empty!");
             alert.show();
             return;
         }
-        for (char c : plainText.toCharArray())
-            if(!Character.isUpperCase(c)){
+        ArrayList<Character>[] rails = new ArrayList[key];
+        for (int i = 0; i < key; i++) {
+            rails[i] = new ArrayList<>();
+        }
+        int markerAddress = 0;
+        boolean reverseFlag = false;
+        for (char c : plainText.toCharArray()) {
+            if (Character.isSpace(c)) {
+                rails[markerAddress].add('x');
+            } else if (!Character.isUpperCase(c)) {
                 alert.setContentText("Your plain text can only contain upper case Alphabetic from A to Z!");
                 alert.show();
                 return;
+            } else {
+                rails[markerAddress].add(c);
             }
+            if (reverseFlag) {
+                markerAddress--;
+                if (markerAddress < 0) {
+                    markerAddress = 1;
+                    reverseFlag = false;
+                }
+            } else {
+                markerAddress++;
+                if (markerAddress == key) {
+                    markerAddress -= 2;
+                    reverseFlag = true;
+                }
+            }
+
+        }
+        String cipher = "";
+        for (ArrayList<Character> a : rails) {
+            for (Character character : a) {
+                cipher += character;
+
+            }
+            System.out.println(cipher);
+        }
+        encCipherTextArea.setText(cipher);
+
+    }
+
+    @FXML
+    void copyCipher() {
+        decCipherTextArea.setText(encCipherTextArea.getText());
+    }
+
+    @FXML
+    void decrypt(ActionEvent event) {
+
     }
 }
